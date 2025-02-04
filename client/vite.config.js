4b2e6ad1469c7913f519af103afbdbcf3ef33a8e
heyaman003 <heyaman329@gmail.com>
2025-02-04 12:27:25 +0530

    chnqage done

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path' // Import the path module
 
 export default defineConfig({
   plugins: [react()],
-  base: "/email-automation", // Ensure this matches your BrowserRouter basename
+  base: '/email-automation/', // Ensure this matches your BrowserRouter basename
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // Use path to define aliases
